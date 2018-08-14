24934b686ba757ed08b1f7ac0b22e6d531360237
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2018-08-14 16:51:37 +0200

    Creating signals and slots to perform the refresh of the main window when something is changed in the current filesystem

--- buttermanager/buttermanager.py
+++ buttermanager/buttermanager.py
@@ -22,10 +22,10 @@ import sys
 import filesystem.filesystem
 import util.utils
 import util.settings
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDesktopWidget, QDialog, QMessageBox
 from PyQt5.QtGui import QCursor
-from PyQt5.QtCore import Qt
-from PyQt5 import uic
+from PyQt5 import uic, QtCore
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
 
 
 class PasswordWindow(QMainWindow):
@@ -103,6 +103,8 @@ class ButtermanagerMainWindow(QMainWindow):
         self.__current_filesystem = None
         # Current filesystem uuid (it will be initialize in initialize method)
         self.__current_filesystem_uuid = None
+        # Balancer that will balance the current filesystem if it is needed
+        self.__balancer = None
         # Initializing the application
         self.init_ui()
 
@@ -152,25 +154,54 @@ class ButtermanagerMainWindow(QMainWindow):
 
         """
         # Displaying info
-        info_dialog = InfoWindow(self, "In progress: please wait until filesystem is balanced. "
-                                       "It will take a little bit...")
-        info_dialog.show()
-        filesystem.filesystem.balance_filesystem(
-            filesystem.filesystem.BTRFS_BALANCE_DATA_USAGE_FILTER,
-            self.__current_filesystem.data_percentage,
-            self.__current_filesystem.mounted_points[0])
-        filesystem.filesystem.balance_filesystem(
-            filesystem.filesystem.BTRFS_BALANCE_METADATA_USAGE_FILTER,
-            self.__current_filesystem.metadata_percentage,
-            self.__current_filesystem.mounted_points[0])
+        # info_dialog = InfoWindow(self, "In progress: please wait until filesystem is balanced. "
+        #                                "It will take a little bit...")
+        # info_dialog.show()
+        # filesystem.filesystem.balance_filesystem(
+        #     filesystem.filesystem.BTRFS_BALANCE_DATA_USAGE_FILTER,
+        #     self.__current_filesystem.data_percentage,
+        #     self.__current_filesystem.mounted_points[0])
+        # filesystem.filesystem.balance_filesystem(
+        #     filesystem.filesystem.BTRFS_BALANCE_METADATA_USAGE_FILTER,
+        #     self.__current_filesystem.metadata_percentage,
+        #     self.__current_filesystem.mounted_points[0])
 
         # Closing info
-        info_dialog.hide()
-        info_dialog.close()
+        # info_dialog.hide()
+        # info_dialog.close()
 
         # Calculating new values for the filesystem balanced
-        self.__current_filesystem = filesystem.filesystem.Filesystem(self.__current_filesystem_uuid)
+        # self.__current_filesystem = filesystem.filesystem.Filesystem(self.__current_filesystem_uuid)
         # Displaying all the info related to the filesystem recently balanced
+        # self.fill_filesystem_info(self.__current_filesystem)
+        self.__balancer = BalanceThread(self.__current_filesystem.data_percentage,
+                                        self.__current_filesystem.mounted_points[0])
+        self.__balancer.show_one_window.connect(self.manage_window)
+        self.__balancer.refresh_filesystem_statistics.connect(self.refresh_filesystem_statistics)
+        self.__balancer.start()
+
+    def manage_window(self, hide):
+        """Shows or hide the main window
+
+        If some important operation (like, f.e. balancing the current filesystem) is being done, then
+        the main window should be hidden in order to avoid the user click on any other button and run
+        another operation until the current is finished.
+
+        Arguments:
+            hide (boolean): Hide or show the main window.
+        """
+        if hide:
+            self.hide()
+        else:
+            self.show()
+
+    def refresh_filesystem_statistics(self):
+        """Refresh current filesystem statistics in the GUI.
+
+        """
+        # Calculating new values for the current filesystem
+        self.__current_filesystem = filesystem.filesystem.Filesystem(self.__current_filesystem_uuid)
+        # Displaying all the info related to the current filesystem
         self.fill_filesystem_info(self.__current_filesystem)
 
     def fill_filesystem_info(self, filesystem):
@@ -179,8 +210,8 @@ class ButtermanagerMainWindow(QMainWindow):
         Arguments:
             filesystem (obj: Filesystem): Filesystem.
         """
-        tooltip = "More info about the filesystem: \n " \
-                  "Devices: {devices} \n " \
+        tooltip = "More info about the filesystem: \n \n" \
+                  "Devices: {devices} \n" \
                   "Mounted points: {mounted_points}".format(devices=filesystem.devices,
                                                             mounted_points=filesystem.mounted_points)
         self.label_filesystem_info_more.setToolTip(tooltip)
@@ -191,14 +222,82 @@ class ButtermanagerMainWindow(QMainWindow):
         self.progressbar_metadata.setValue(filesystem.metadata_percentage)
         self.progressbar_system.setValue(filesystem.system_percentage)
 
+
+class BalanceThread(QThread):
+    """Indepented thread that will run the filesystem balancing process.
+
+    """
+
+    # Attributes
+    # pyqtSignal that will be emitted when this class requires to display
+    # a single information window on the screen
+    show_one_window = pyqtSignal('bool')
+    # pyqtSignal that will be emitted when this class requires that main
+    # window refreshes current filesystem statistics
+    refresh_filesystem_statistics = pyqtSignal()
+
+    def __init__(self, percentage, mounted_point):
+        QThread.__init__(self)
+        self.__percentage = percentage
+        self.__mounted_point = mounted_point
+
+    def run(self):
+        # Main window will be hidden
+        self.on_show_one_window(True)
+        info_dialog = InfoWindow(None, "Balancing '{mounted_point}' mounted point. \n"
+                                       "This window will be closed automatically \n"
+                                       "when the operation is done. \n \n"
+                                       "Please wait...".format(mounted_point=self.__mounted_point))
+        # Displaying info window
+        info_dialog.show()
+
+        # Balances the filesystem
+        self.__balance_filesystem()
+
+        # Hiding info window
+        info_dialog.hide()
+
+        # Main window will be shown again
+        self.on_show_one_window(False)
+
+        # Refreshing current filesystem statistics
+        self.on_refresh_filesystem_statistics()
+
+    def __balance_filesystem(self):
+        """Wraps all the operations to balance the filesystem.
+
+        """
+        filesystem.filesystem.balance_filesystem(
+            filesystem.filesystem.BTRFS_BALANCE_DATA_USAGE_FILTER,
+            self.__percentage,
+            self.__mounted_point)
+        filesystem.filesystem.balance_filesystem(
+            filesystem.filesystem.BTRFS_BALANCE_METADATA_USAGE_FILTER,
+            self.__percentage,
+            self.__mounted_point)
+
+
+    def on_show_one_window(self, one_window):
+        """Emits a QT Signal to hide or show the rest of application windows.
+
+        Arguments:
+            one_window (boolean): Information window should be unique?.
+        """
+        self.show_one_window.emit(one_window)
+
+    def on_refresh_filesystem_statistics(self):
+        """Emits a QT Signal to refresh filesystem statistics in main window.
+
+        """
+        self.refresh_filesystem_statistics.emit()
+
 class InfoWindow(QDialog):
     """Window to display information.
 
-    Class inherited from QDialog (Window constructor)
     """
     # Constructor
     def __init__(self, parent, information):
-        QDialog.__init__(self, parent)
+        QDialog.__init__(self, parent, QtCore.Qt.WindowStaysOnTopHint)
         self.parent = parent
         # Initializing the window
         self.init_ui(information)
