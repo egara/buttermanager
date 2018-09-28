58f6eada5997e6ed4bc8cc9eae9265d4d171d71f
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2018-09-28 09:00:58 +0200

    Adding extra check when user wants to upgrade the system. If there is no package to update, then the process won't be triggered

--- buttermanager/manager/upgrader.py
+++ buttermanager/manager/upgrader.py
@@ -30,6 +30,7 @@ from PyQt5.QtCore import QThread, pyqtSignal
 import util.utils
 import util.settings
 
+ARCH_PACMAN_CHECK_UPDATES = "sudo -S pacman -Qu"
 ARCH_PACMAN_UPGRADE_COMMAND = "sudo -S pacman -Syu --noconfirm"
 DEBIAN_APT_UPDATE_COMMAND = "sudo -S apt update"
 DEBIAN_APT_UPGRADE_COMMAND = "sudo -S apt upgrade -y"
@@ -78,80 +79,91 @@ class Upgrader(QThread):
         """Wraps all the operations to upgrade the system.
 
         """
-        # Starting the upgrading process. Disabling all the buttons.
-        self.on_disable_gui_buttons()
+        # Check for updates
+        if self.__check_updates():
+            # There are system updates
+            # Starting the upgrading process. Disabling all the buttons.
+            self.on_disable_gui_buttons()
 
-        self.__logger.info("Starting system upgrading process.")
-        sys.stdout.write("Starting system upgrading process. Please wait...")
-        sys.stdout.write("\n")
-        sys.stdout.write("--------")
-        sys.stdout.write("\n")
-
-        # Creates all the snapshots needed before upgrading the system
-
-        for snapshot in util.settings.subvolumes:
-            snapshot.create_snapshot()
-
-        # Upgrades the system
-        upgrading_command = ""
-        if util.settings.user_os == util.utils.OS_ARCH:
-            upgrading_command = ARCH_PACMAN_UPGRADE_COMMAND
-        elif util.settings.user_os == util.utils.OS_DEBIAN:
-            # First, it is necessary to update the system
             sys.stdout.write("\n")
             sys.stdout.write("--------")
             sys.stdout.write("\n")
-            sys.stdout.write("Updating the system. Please wait...")
-            sys.stdout.write("\n")
-            util.utils.execute_command(DEBIAN_APT_UPDATE_COMMAND, console=True)
-            sys.stdout.write("\n")
-            upgrading_command = DEBIAN_APT_UPGRADE_COMMAND
-
-        sys.stdout.write("Upgrading the system. Please wait...")
-        sys.stdout.write("\n")
-        util.utils.execute_command(upgrading_command, console=True)
-
-        # Upgrades AUR if distro is ArchLinux or derivatives
-        if util.settings.user_os == util.utils.OS_ARCH:
+            self.__logger.info("Starting system upgrading process.")
+            sys.stdout.write("Starting system upgrading process. Please wait...")
             sys.stdout.write("\n")
             sys.stdout.write("--------")
             sys.stdout.write("\n")
-            sys.stdout.write("Updating AUR packages if it is needed. Please wait...")
-            sys.stdout.write("\n")
-            if util.utils.exist_program(ARCH_YAY_COMMAND):
-                util.utils.execute_command(ARCH_YAY_UPGRADE_COMMAND, console=True)
-            elif util.utils.exist_program(ARCH_TRIZEN_COMMAND):
-                util.utils.execute_command(ARCH_TRIZEN_UPGRADE_COMMAND, console=True)
-            elif util.utils.exist_program(ARCH_YAOURT_COMMAND):
-                util.utils.execute_command(ARCH_YAOURT_UPGRADE_COMMAND, console=True)
 
-        # Upgrades snap packages
-        if util.utils.exist_program(SNAP_COMMAND):
-            sys.stdout.write("\n")
-            sys.stdout.write("--------")
-            sys.stdout.write("\n")
-            sys.stdout.write("Updating snaps. Please wait...")
-            sys.stdout.write("\n")
-            util.utils.execute_command(SNAP_UPGRADE_COMMAND, console=True)
+            # Creates all the snapshots needed before upgrading the system
 
-        # Removes all the snapshots not needed any more it it is needed
-        if not self.__dont_remove_snapthosts:
-            sys.stdout.write("\n")
-            sys.stdout.write("--------")
-            sys.stdout.write("\n")
-            sys.stdout.write("Removing old snapshots if it is needed. Please wait...")
-            sys.stdout.write("\n")
             for snapshot in util.settings.subvolumes:
-                snapshot.delete_snapshots()
+                snapshot.create_snapshot()
 
-        sys.stdout.write("\n")
-        sys.stdout.write("--------")
-        sys.stdout.write("\n")
-        self.__logger.info("System upgrading process finished.")
-        sys.stdout.write("System upgrading process finished. You can close the terminal output now.")
+            # Upgrades the system
+            upgrading_command = ""
+            if util.settings.user_os == util.utils.OS_ARCH:
+                upgrading_command = ARCH_PACMAN_UPGRADE_COMMAND
+            elif util.settings.user_os == util.utils.OS_DEBIAN:
+                # First, it is necessary to update the system
+                sys.stdout.write("\n")
+                sys.stdout.write("--------")
+                sys.stdout.write("\n")
+                sys.stdout.write("Updating the system. Please wait...")
+                sys.stdout.write("\n")
+                util.utils.execute_command(DEBIAN_APT_UPDATE_COMMAND, console=True)
+                sys.stdout.write("\n")
+                upgrading_command = DEBIAN_APT_UPGRADE_COMMAND
 
-        # Refreshing GUI
-        self.on_refresh_gui()
+            sys.stdout.write("Upgrading the system. Please wait...")
+            sys.stdout.write("\n")
+            util.utils.execute_command(upgrading_command, console=True)
+
+            # Upgrades AUR if distro is ArchLinux or derivatives
+            if util.settings.user_os == util.utils.OS_ARCH:
+                sys.stdout.write("\n")
+                sys.stdout.write("--------")
+                sys.stdout.write("\n")
+                sys.stdout.write("Updating AUR packages if it is needed. Please wait...")
+                sys.stdout.write("\n")
+                if util.utils.exist_program(ARCH_YAY_COMMAND):
+                    util.utils.execute_command(ARCH_YAY_UPGRADE_COMMAND, console=True)
+                elif util.utils.exist_program(ARCH_TRIZEN_COMMAND):
+                    util.utils.execute_command(ARCH_TRIZEN_UPGRADE_COMMAND, console=True)
+                elif util.utils.exist_program(ARCH_YAOURT_COMMAND):
+                    util.utils.execute_command(ARCH_YAOURT_UPGRADE_COMMAND, console=True)
+
+            # Upgrades snap packages
+            if util.utils.exist_program(SNAP_COMMAND):
+                sys.stdout.write("\n")
+                sys.stdout.write("--------")
+                sys.stdout.write("\n")
+                sys.stdout.write("Updating snaps. Please wait...")
+                sys.stdout.write("\n")
+                util.utils.execute_command(SNAP_UPGRADE_COMMAND, console=True)
+
+            # Removes all the snapshots not needed any more it it is needed
+            if not self.__dont_remove_snapthosts:
+                sys.stdout.write("\n")
+                sys.stdout.write("--------")
+                sys.stdout.write("\n")
+                sys.stdout.write("Removing old snapshots if it is needed. Please wait...")
+                sys.stdout.write("\n")
+                for snapshot in util.settings.subvolumes:
+                    snapshot.delete_snapshots()
+
+            sys.stdout.write("\n")
+            sys.stdout.write("--------")
+            sys.stdout.write("\n")
+            self.__logger.info("System upgrading process finished.")
+            sys.stdout.write("System upgrading process finished. You can close the terminal output now.")
+
+            # Refreshing GUI
+            self.on_refresh_gui()
+
+        else:
+            # There are not system updates
+            self.__logger.info("There are not system updates.")
+            sys.stdout.write("There are not system updates. You can close the terminal output now.")
 
         # Finishing the upgrading process. Enabling all the buttons.
         self.on_enable_gui_buttons()
@@ -173,3 +185,25 @@ class Upgrader(QThread):
 
         """
         self.refresh_gui.emit()
+
+    def __check_updates(self):
+        self.__logger.info("Checking for system updates.")
+        sys.stdout.write("Checking for system updates.")
+        sys.stdout.write("\n")
+        sys.stdout.write("--------")
+        sys.stdout.write("\n")
+
+        updates = False
+        if util.settings.user_os == util.utils.OS_ARCH:
+            check_for_updates_command = ARCH_PACMAN_CHECK_UPDATES
+            commandline_output = util.utils.execute_command(check_for_updates_command)
+
+            for line in commandline_output.split("\n"):
+                if line:
+                    updates = True
+
+        else:
+            # TODO: Take into account Debian based distro
+            updates = True
+
+        return updates
