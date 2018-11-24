469c51ea87b00cc53cd9d392b9cb10fb999fd490
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2018-11-24 16:05:54 +0100

    Upgrading setup.py to take into account requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,29 @@
 import setuptools
+import pip
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+links = []
+requires = []
+
+try:
+    requirements = pip.req.parse_requirements('requirements.txt')
+except:
+    # new versions of pip requires a session
+    requirements = pip.req.parse_requirements(
+        'requirements.txt', session=pip.download.PipSession())
+
+for item in requirements:
+    # we want to handle package names and also repo urls
+    if getattr(item, 'url', None):  # older pip has url
+        links.append(str(item.url))
+    if getattr(item, 'link', None): # newer pip has link
+        links.append(str(item.link))
+    if item.req:
+        requires.append(str(item.req))
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,6 +38,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/egara/buttermanager",
     packages=setuptools.find_packages(),
+    install_requires=requires
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
