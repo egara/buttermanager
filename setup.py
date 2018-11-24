328e8c56692ba6a8e19ec049b12683755c6e43a1
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2018-11-24 09:16:34 +0100

    Adding some files to package the application

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="buttermanager",
+    version="1.0",
+    author="Eloy García Almadén",
+    author_email="eloy.garcia.pca@gmail.com",
+    description="BTRFS tool for managing snapshots, balancing filesystems and upgrading the system safetly",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/egara/buttermanager",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 4 - Beta",
+        "Environment :: X11 Applications :: Qt",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: System :: Filesystems",
+        "Topic :: Utilities"
+    ],
+)
