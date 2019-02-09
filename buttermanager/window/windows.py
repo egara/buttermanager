b689b9239ab5cdc957622c9abb11a710064393c5
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2019-02-09 16:33:25 +0100

    Adding some text to check. It has to be removed

--- buttermanager/window/windows.py
+++ buttermanager/window/windows.py
@@ -344,6 +344,9 @@ class UpdatesWindow(QMainWindow):
         elif util.settings.user_os == util.utils.OS_DEBIAN:
             check_for_updates_command = manager.upgrader.DEBIAN_APT_CHECK_UPDATES
             commandline_output = util.utils.execute_command(check_for_updates_command)
+            lines = commandline_output.split("\n");
+            print("Heyyyyyyyyy:" + lines)
+            print("Number of lines:" + len(lines))
 
         for line in commandline_output.split("\n"):
             self.text_edit_console.moveCursor(QTextCursor.End)
