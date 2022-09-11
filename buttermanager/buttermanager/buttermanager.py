1da86bbe2fb371ef48116bc0d9ab3f41167c46a8
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2022-09-11 08:31:40 +0200

    Now, these labels will be added to the rest in order to scale them with the same form factor

--- buttermanager/buttermanager/buttermanager.py
+++ buttermanager/buttermanager/buttermanager.py
@@ -268,11 +268,11 @@ class ButtermanagerMainWindow(QMainWindow):
                                   self.checkbox_snap, self.checkbox_aur, self.button_save_log,
                                   self.button_close_terminal, self.button_wiki, self.label_documentation,
                                   self.checkbox_grub_btrfs, self.button_regenerate_grub, self.label_settings_ui,
-                                  self.label_font_size, self.spinbox_font_size]
-            utils.scale_fonts(self.__ui_elements)
-            self.__ui_elements = [self.label_settings_subvolumes_where, self.label_settings_subvolumes_prefix,
+                                  self.label_font_size, self.spinbox_font_size, self.label_settings_subvolumes_where,
+                                  self.label_settings_subvolumes_prefix,
                                   self.label_settings_subvolumes_snapshots_to_keep]
-            utils.scale_fonts(self.__ui_elements, 2)
+            utils.scale_fonts(self.__ui_elements, -2)
+
             # Tooltips
             self.setStyleSheet(" QToolTip{font: " + str(settings.base_font_size) + "pt}")
 
